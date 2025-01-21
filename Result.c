faa6f31d27a8b0997b7a2340d9e3191fed894aa3
Chi-Kuan Chiu <devaraja1945@gmail.com>
2025-01-21 19:22:12 +0800

    Init

--- Result.c
+++ Result.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "queue.h"
+#include "ringbuffer.h"
 
 int main(){
-    queue_t Q;
-    queue_init(&Q,sizeof(record_item)*9000);
-    while(1){
-        record_item tmp;
-        if(!queue_get(&Q, &tmp, sizeof(record_item))) break;
-        printf("Action : %s, Time : %ld, Memory Usage %lld \n",tmp.op_name, tmp.op_time, tmp.Memory_usage);
-
+    ringbuf_shm_t ringbuf_shm;
+    assert(ringbuf_shm_init(&ringbuf_shm, "/ringbuf_shm_test", ARRAY_LENGTH*8, true) == 0);
+    int shm_fd;
+    record_item *shared_array;
+    shm_fd = shm_open("/shm_array", O_RDONLY, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(1);
     }
-    queue_destroy(&Q);
+    shared_array = mmap(NULL, sizeof(record_item) * ARRAY_LENGTH, PROT_READ, MAP_SHARED, shm_fd, 0);
+    if (shared_array == MAP_FAILED) {
+        perror("mmap failed");
+        close(shm_fd);
+        exit(1);
+    }
+    uint64_t prev = 0;
+    uint64_t itr = 100000;
+    
+    while (itr --)
+    {
+        Reading(ringbuf_shm.ringbuf, shared_array,shm_fd, &prev);
+    }
+    printf("End\n");
     return 0;
 }
